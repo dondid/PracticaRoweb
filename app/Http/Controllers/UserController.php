2645b7cfbbf39f200edb2f5011642a32cd031f66
Daniel <ionutdanieldodoc@gmail.com>
2022-07-10 18:53:20 +0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ *
+ */
+class UserController extends ApiController
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Bad request!', $validator->messages()->toArray());
+        }
+
+        $error = false;
+
+        /** @var User $user */
+        $user = User::where('email', $request->get('email'))->first();
+        if (!$user) {
+            $error = true;
+        } else {
+            if (!Hash::check($request->get('password'), $user->password)) {
+                $error = true;
+            }
+        }
+
+        if ($error) {
+            return $this->sendError('Bad credentials!');
+        }
+
+        $token = $user->createToken('Practica');
+
+        return $this->sendResponse([
+            'token' => $token->plainTextToken,
+            'user' => $user->toArray()
+        ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Bad request!', $validator->messages()->toArray());
+        }
+
+        $user=new User();
+        $user->name=$request->get ('name');
+        $user->name=$request->get ('email');
+        $user->name=Hash::make($request->get ('password'));
+        $user->save();
+
+        $token = $user->createToken('Practica');
+
+        return $this->sendResponse([
+            'token' => $token->plainTextToken,
+            'user' => $user->toArray()
+        ]);
+    }
+}
